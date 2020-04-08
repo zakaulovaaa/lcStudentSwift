ced0330ad79a9c65cd95fd025ceb0ad3c69d529b
zakaulovaaa <darya.zakaulova@yandex.ru>
2020-04-08 20:13:55 +0800

    Initial Commit

---
+++ lc_student/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lc_student
+//
+//  Created by Дарья Закаулова on 08.04.2020.
+//  Copyright © 2020 Дарья Закаулова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
